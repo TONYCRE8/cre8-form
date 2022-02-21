5f52bf6900cbae82e04912e4815d33769845ed8c
TONYCRE8 <tonyingall.business@gmail.com>
2022-02-21 14:08:28 +0000

    update config js

--- src/config.js
+++ src/config.js
@@ -4,11 +4,12 @@ var config = {
     messages: {
         elementClass: 'status-message',
         success: 'Thank you very much! We\'ve got your message and will be in touch with you soon.',
-        reject: 'Sorry, something went wrong. Try refreshing and filling out the form again.'
+        reject: 'Sorry, something went wrong. Try refreshing and filling out the form again.',
+        scrollTo: false // If you want the page to scroll up after submission
     },
     intlTelInput: {
         enabled: true, // disabling this will default to using RFC 3966 as validation.
-        preferred_countries: ['gb', 'us', 'ca'], // used with intlTelInput
+        preferredCountries: ['gb', 'us', 'ca'], // used with intlTelInput
         autolocation: { // find out more here: https://github.com/jackocnr/intl-tel-input#initialisation-options
             enabled: false,
             url: ''

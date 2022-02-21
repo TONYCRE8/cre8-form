028773c954a87a32f2b7b980afe837f9c26d2d6e
TONYCRE8 <tonyingall.business@gmail.com>
2022-02-21 14:01:27 +0000

    create config file

---
+++ src/config.js
@@ -0,0 +1,18 @@
+var config = {
+    // defaults
+    privacy: true,
+    messages: {
+        elementClass: 'status-message',
+        success: 'Thank you very much! We\'ve got your message and will be in touch with you soon.',
+        reject: 'Sorry, something went wrong. Try refreshing and filling out the form again.'
+    },
+    intlTelInput: {
+        enabled: true, // disabling this will default to using RFC 3966 as validation.
+        preferred_countries: ['gb', 'us', 'ca'], // used with intlTelInput
+        autolocation: {
+            enabled: false,
+            url: ''
+        }
+    },
+
+}
